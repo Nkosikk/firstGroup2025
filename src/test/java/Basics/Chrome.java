b68bce906e315cd43df3e18d65b4a17da803b8cc
Mahlatse03 <mahlatsed@gmail.com>
2024-12-10 19:27:26 +0200

    Moved Chrome driver class to New folder

--- src/test/java/Basics/Chrome.java
+++ src/test/java/Basics/Chrome.java
@@ -1,3 +1,5 @@
+package Basics;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -5,7 +7,6 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 public class Chrome {
@@ -14,28 +15,28 @@ public class Chrome {
     @Test
     public void loginToSauceDemoWithBrowser() {
         driver = new ChromeDriver();
-        driver = new FirefoxDriver();
-        driver = new EdgeDriver();
-        driver = new SafariDriver();
+//        driver = new FirefoxDriver();
+//        driver = new EdgeDriver();
+//        driver = new SafariDriver();
         driver.get("https://www.saucedemo.com/");
         driver.manage().window().maximize();
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.xpath("//input[@id='login-button']")).click();
 
-        driver.findElement(By.xpath("//span[contains(.,'Products')]")).isDisplayed();
+       // driver.findElement(By.xpath("//span[contains(.,'Products')]")).isDisplayed();
 
-//        String productText = driver.findElement(By.xpath("//span[contains(.,'Products')]")).getText();
-//
-//        Assert.assertEquals(productText,"Products");
+        String productText = driver.findElement(By.xpath("//span[contains(.,'Products')]")).getText();
+
+        Assert.assertEquals(productText,"Products");
 
 
 
     }
 
-    @AfterTest
-    public void closeBrowser() {
-        driver.quit();
-    }
+//    @AfterTest
+//    public void closeBrowser() {
+//        driver.quit();
+//    }
 
 }
