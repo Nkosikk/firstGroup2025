95d0e6488122d307642c3e298214b298b71f0e7c
nkosi <nkosi.cele@simplify.africa>
2024-12-10 18:23:38 +0200

    added new lines to start more browser

--- src/test/java/Chrome.java
+++ src/test/java/Chrome.java
@@ -1,6 +1,9 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
@@ -11,6 +14,9 @@ public class Chrome {
     @Test
     public void loginToSauceDemoWithBrowser() {
         driver = new ChromeDriver();
+        driver = new FirefoxDriver();
+        driver = new EdgeDriver();
+        driver = new SafariDriver();
         driver.get("https://www.saucedemo.com/");
         driver.manage().window().maximize();
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
