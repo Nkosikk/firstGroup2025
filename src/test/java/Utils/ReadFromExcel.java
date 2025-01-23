0103aba296abdfc814b2b8b81c6ba844b532c4a7
nkosi <nkosi.cele@simplify.africa>
2025-01-23 19:19:46 +0200

    reading data from excel

--- src/test/java/Utils/ReadFromExcel.java
+++ src/test/java/Utils/ReadFromExcel.java
@@ -1,14 +1,30 @@
 package Utils;
 
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class ReadFromExcel {
 
     private static String testDataDir = System.getProperty("user.dir") + "/src/test/java/TestData/data.xlsx";
 
+    FileInputStream fis = new FileInputStream(testDataDir);
+    XSSFWorkbook workbook = new XSSFWorkbook(fis);
+
+    public ReadFromExcel() throws IOException {
+    }
+
+    XSSFSheet sheet = workbook.getSheet("login Details");
+
+    public String username = sheet.getRow(1).getCell(0).getStringCellValue();
 
     @Test
-    public void test(){
-        System.out.println(testDataDir);
+    public void test() {
+        System.out.println(username);
     }
+
+
 }
