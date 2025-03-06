02a27d2aa85ab66e4c123829467f608bf1203c9e
sibonisot <sibonisot@yahoo.com>
2025-03-06 19:43:05 +0200

    added headless run

--- src/test/java/Utils/BrowserFactory.java
+++ src/test/java/Utils/BrowserFactory.java
@@ -2,6 +2,7 @@ package Utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
@@ -16,7 +17,9 @@ public class BrowserFactory {
     public static WebDriver startBrowser(String browserChoice, String url) {
 
         if (browserChoice.equalsIgnoreCase("chrome")) {
-            driver = new ChromeDriver();
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("--headless");
+            driver = new ChromeDriver(chromeOptions);
         } else if (browserChoice.equalsIgnoreCase("firefox")) {
             driver = new FirefoxDriver();
         } else if (browserChoice.equalsIgnoreCase("edge")) {
